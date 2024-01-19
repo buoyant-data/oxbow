9dda9792380ddc6710621ce9ee1039fe222880d5
R. Tyler Croy <rtyler@brokenco.de>
2024-01-18 21:06:57 -0800

    Add a simplistic lambda for reporting version statuses as tables are updated

--- lambdas/deltadog/src/main.rs
+++ lambdas/deltadog/src/main.rs
@@ -1,11 +1,19 @@
 use aws_lambda_events::event::sqs::SqsEvent;
+use dipstick::*;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use tracing::log::*;
+use url::Url;
 
 use oxbow_lambda_shared::*;
 
 async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
-    debug!("Receiving event: {:?}", event);
+    let statsd = std::env::var("STATSD_HOST").unwrap_or("127.0.0.1:8125".into());
+    let prefix = std::env::var("STATSD_PREFIX").unwrap_or(String::new());
+
+    info!("Configured for statsd events on {statsd} with the prefix of \"{prefix}\"");
+    let metrics = Statsd::send_to(statsd)?.named(prefix).metrics();
+
+    debug!("Receiving event: {event:?}");
     let records = s3_from_sqs(event.payload)?;
     debug!("processing records: {records:?}");
     let records = records_with_url_decoded_keys(&records);
@@ -20,7 +28,18 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> Result<(), Error> {
         match deltalake::open_table(table_name).await {
             Ok(table) => {
                 debug!("Opened table {table:?} for metrics tracking");
-            },
+                let metric_name = url_to_metric_name(table_name);
+
+                let version = metrics.gauge(&format!("deltadog.{metric_name}.version"));
+                let table_version = table.version();
+                metrics
+                    .observe(version, move |_| {
+                        table_version
+                            .try_into()
+                            .expect("Failed to convert i64 to isize")
+                    })
+                    .on_flush();
+            }
             Err(e) => {
                 error!("Failed to open Delta table: {e:?}");
             }
@@ -42,4 +61,32 @@ async fn main() -> Result<(), Error> {
 
     info!("Starting deltadog");
     run(service_fn(function_handler)).await
+}
+
+/// Trying to convert the given url into a statsd compatible metric string
+fn url_to_metric_name<S: Into<String>>(url: S) -> String {
+    let url = Url::parse(&url.into())
+        .expect("Failed to turn a URL into a metric because it's not a real URL!");
+
+    let hostname: String = match url.host_str() {
+        Some(hostname) => hostname.to_string(),
+        None => String::new(),
+    };
+    format!(
+        "{}{}",
+        hostname.replace('-', ""),
+        url.path().replace('-', "").replace('/', "_")
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn s3_url_to_metric_name() {
+        let s = "s3://mybucket-name/databases/mylogs/my-table";
+        let metric = "mybucketname_databases_mylogs_mytable";
+        assert_eq!(url_to_metric_name(s), metric);
+    }
 }

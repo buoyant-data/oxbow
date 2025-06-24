7cb797bdf273ed9e429f123ba47ad88ff574b956
R. Tyler Croy <rtyler@brokenco.de>
2025-06-24 15:03:31 +0000

    Utilize Datafusion to produce the output CSV files from the change data
    This commit removes some of Eugene's earlier work for translating types
    for import. There are CsvWriterOptions which can handle the timestamp
    conversion but I will need to do some more integration testing to see
    whether any of these manual type conversions are needed with the
    Datafusion csv writer

--- lambdas/cdf-to-csv/src/main.rs
+++ lambdas/cdf-to-csv/src/main.rs
@@ -1,32 +1,25 @@
-use aws_config;
+//!
+//! This lambda function should be triggered by S3 Bucket Notifications on Delta tables and will
+//! produce CSV files to ingest into Aurora for Change Data Feeds
+
 use aws_lambda_events::event::sqs::SqsEvent;
-use aws_lambda_events::s3::S3EventRecord;
-use aws_sdk_s3;
+use deltalake::datafusion::dataframe::DataFrameWriteOptions;
 use deltalake::datafusion::prelude::*;
 use deltalake::delta_datafusion::DeltaCdfTableProvider;
-use deltalake::parquet::file::reader::FileReader;
-use deltalake::parquet::file::serialized_reader::SerializedFileReader;
-use deltalake::parquet::record::RowAccessor;
 use deltalake::{DeltaOps, DeltaResult};
 use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
+use object_store::prefix::PrefixStore;
 use oxbow_lambda_shared::*;
 use std::sync::Arc;
 use tracing::log::*;
-
-mod mysql_value;
-mod s3_upload;
-use mysql_value::ToMysqlValue;
-
-const CHANGE_TYPE_COLUMN: &str = "_change_type";
-const INSERT: &str = "insert";
-const UPDATE_POSTIMAGE: &str = "update_postimage";
-const DELETE: &str = "delete";
+use url::Url;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     deltalake::aws::register_handlers(None);
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
@@ -34,15 +27,12 @@ async fn main() -> Result<(), Error> {
         .init();
 
     info!("Starting cdf-to-csv");
-
     run(service_fn(function_handler)).await
 }
 
 async fn function_handler(event: LambdaEvent<SqsEvent>) -> DeltaResult<(), Error> {
     debug!("Receiving event: {:?}", event);
-
-    let config = aws_config::load_defaults(aws_config::BehaviorVersion::v2025_01_17()).await;
-    let client = aws_sdk_s3::Client::new(&config);
+    let destination = std::env::var("CSV_OUTPUT_URL").expect("CSV_OUTPUT_URL not set");
 
     let triggers = triggers_from(event.payload)?;
     debug!("Triggered by: {triggers:?}");
@@ -64,6 +54,13 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> DeltaResult<(), Error
 
         if let (Some(mut min), Some(max)) = (trigger.smallest_version(), trigger.largest_version())
         {
+            // Creating a new [SessionContext] for every trigger to make sure the namespace inside
+            // the context is unique to a triggered table
+            let ctx = SessionContext::new();
+            let destination = Url::parse(&destination).expect("Failed to parse the CSV_OUTPUT_URL");
+            let (store, _path) = object_store::parse_url(&destination)
+                .expect("Failed to create an output object_store");
+
             let table = deltalake::open_table(trigger.location().as_str()).await?;
             info!(
                 "Loaded a table for {} at version {}",
@@ -77,16 +74,28 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> DeltaResult<(), Error
                 min = max - 1;
             }
 
+            // The prefix used should be bespoke for every table trigger and contain the max
+            // version processed to make sure there are no conflicts between invocations
+            let prefix = format!("{}/{}/inserts", trigger.location().path(), max);
+            let store = Arc::new(PrefixStore::new(store, prefix));
+            // Registering our destination into the [SessionContext] so that write_csv() can just use our
+            // cdf output scheme for writing into S3
+            ctx.register_object_store(
+                &Url::parse("cdfo://").expect("Failed to parse internal URI scheme"),
+                store.clone(),
+            );
+
             // Always look at the last version
             let cdf = DeltaOps::from(table)
                 .load_cdf()
                 .with_starting_version(min)
                 .with_ending_version(max);
-            let ctx = SessionContext::new();
-            let provider = Arc::new(DeltaCdfTableProvider::try_new(cdf)?);
-            let df = ctx.read_table(provider)?;
-            df.show().await?;
-            // TODO: take this data frame and turn it into CSV files
+            let provider = DeltaCdfTableProvider::try_new(cdf)?;
+
+            let change_data = retrieve_change_data(&ctx, provider).await?;
+            change_data
+                .write_csv("cdfo://", DataFrameWriteOptions::default(), None)
+                .await?;
         } else {
             warn!("Invoked but didn't find min/max trigger versions, something is fishy!");
         }
@@ -94,172 +103,100 @@ async fn function_handler(event: LambdaEvent<SqsEvent>) -> DeltaResult<(), Error
     Ok(())
 }
 
-async fn process_parquet_file(
-    client: &aws_sdk_s3::Client,
-    bucket: &String,
-    key: &String,
-) -> Result<(), Box<dyn std::error::Error>> {
-    info!("Processing s3://{}/{}", bucket, key);
-
-    let body = client
-        .get_object()
-        .bucket(bucket.clone())
-        .key(key.clone())
-        .send()
-        .await
-        .map_err(|e| format!("S3 get object: {:?}", e))?
-        .body
-        .collect()
-        .await
-        .map(|data| data.into_bytes())
-        .map_err(|e| format!("S3 reading body: {:?}", e))?;
-
-    let reader =
-        SerializedFileReader::new(body).map_err(|e| format!("can't read parquet {:?}", e))?;
-
-    let max_rows_per_file = std::env::var("CSV_MAX_LINES_PER_FILE")
-        .unwrap_or_else(|_| "1000000".to_string())
-        .parse::<usize>()
-        .expect("CSV_MAX_LINES_PER_FILE must be a number");
-
-    let bucket = std::env::var("CSV_OUTPUT_BUCKET").expect("CSV_OUTPUT_BUCKET not set");
-    let update_prefix = std::env::var("CSV_OUTPUT_PREFIX").expect("CSV_OUTPUT_PREFIX not set");
-    let delete_prefix = std::env::var("CSV_DELETE_PREFIX").ok();
-    let basename = basename_from_key(key);
-
-    let change_type_index =
-        column_index(&reader, CHANGE_TYPE_COLUMN).ok_or("_change_type column not")?;
-    let primary_key_column = std::env::var("DELETE_PRIMARY_KEY")
-        .ok()
-        .and_then(|name| column_index(&reader, &name));
-
-    let row_iter = reader
-        .get_row_iter(None)
-        .map_err(|e| format!("can't read row: {:?}", e))?;
-
-    let mut updates = s3_upload::S3Upload::new(
-        client,
-        &bucket,
-        &update_prefix,
-        &basename,
-        max_rows_per_file,
-    );
-    let mut deletes: Option<s3_upload::S3Upload> = delete_prefix.map(|prefix| {
-        s3_upload::S3Upload::new(client, &bucket, &prefix, &basename, max_rows_per_file)
-    });
-
-    for row in row_iter {
-        let row = row.map_err(|e| format!("failed reading row: {:?}", e))?;
-
-        let change_type = row.get_string(change_type_index)?;
-        if change_type == INSERT || change_type == UPDATE_POSTIMAGE {
-            let fields = row
-                .get_column_iter()
-                .filter_map(|(name, field)| {
-                    if name != CHANGE_TYPE_COLUMN {
-                        Some(
-                            field
-                                .to_mysql_value()
-                                .map_err(|e| format!("error encoding {}: {:?}", name, e)),
-                        )
-                    } else {
-                        None
-                    }
-                })
-                .collect::<Result<Vec<_>, _>>()?;
-
-            updates.write_record(fields).await?;
-        } else if change_type == DELETE {
-            match (primary_key_column, deletes.as_mut()) {
-                (Some(primary_key_column), Some(ref mut deletes)) => {
-                    let primary_key_value = row
-                        .get_column_iter()
-                        .skip(primary_key_column)
-                        .next()
-                        .ok_or("cannot get primary key, row does not have enough columns")?
-                        .1
-                        .to_mysql_value()
-                        .map_err(|e| {
-                            format!("cannot convert primary key to mysql value: {:?}", e)
-                        })?;
-                    deletes.write_record(vec![primary_key_value]).await?;
-                }
-                _ => {}
-            }
-        }
-    }
-
-    updates.close().await?;
-    if let Some(deletes) = deletes {
-        deletes.close().await?;
-    }
-
-    Ok(())
-}
-
-fn column_index(parquet: &dyn FileReader, column_name: &str) -> Option<usize> {
-    parquet
-        .metadata()
-        .file_metadata()
-        .schema_descr()
-        .columns()
-        .iter()
-        .enumerate()
-        .find(|(_idx, column)| column.name() == column_name)
-        .map(|(idx, _)| idx)
-}
-
-fn basename_from_key(key: &str) -> String {
-    key.split('/')
-        .last()
-        .unwrap()
-        .split('.')
-        .next()
-        .unwrap()
-        .to_string()
-}
-
-#[derive(Debug, PartialEq)]
-enum RecordType {
-    Parquet,
-    Unknown,
-}
-
-fn suffix_from_record(record: &S3EventRecord) -> RecordType {
-    if let Some(key) = record.s3.object.key.as_ref() {
-        if key.ends_with(".parquet") {
-            return RecordType::Parquet;
-        }
-    }
-    if let Some(key) = record.s3.object.url_decoded_key.as_ref() {
-        if key.ends_with(".parquet") {
-            return RecordType::Parquet;
-        }
-    }
-    RecordType::Unknown
+/// This function must be given an existing Apache Datafusiojn [SessionContext] and will produce
+/// a simplistic dataframe with _only_ the changed data from the [DeltaCdfTableProvider].
+///
+/// That is to say, none of the underscore extra columns or metadata
+///
+/// NOTE: Right now this is only handling inserts/updates but **not** deletes. Deletes should be
+/// handled somewhat differently
+async fn retrieve_change_data(
+    ctx: &SessionContext,
+    cdf_provider: DeltaCdfTableProvider,
+) -> DeltaResult<DataFrame> {
+    ctx.register_table("cdf", Arc::new(cdf_provider))?;
+    let df = ctx
+        .sql("SELECT * FROM cdf WHERE _change_type IN ('insert', 'update_postimage')")
+        .await?;
+    Ok(df.drop_columns(&[
+        "_change_type",
+        "_commit_version",
+        "_commit_timestamp",
+        "_change_version",
+    ])?)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use aws_lambda_events::s3::{S3Entity, S3EventRecord, S3Object};
+    use futures::StreamExt;
+    use object_store::ObjectStore;
 
-    #[test]
-    fn test_suffix_from_record() {
-        let d = S3EventRecord::default();
-        assert_eq!(suffix_from_record(&d), RecordType::Unknown);
+    use deltalake::datafusion::{
+        common::assert_batches_sorted_eq, dataframe::DataFrameWriteOptions,
+    };
+    use deltalake::operations::load_cdf::CdfLoadBuilder;
 
-        let jsonl = S3EventRecord {
-            s3: S3Entity {
-                object: S3Object {
-                    key: Some("some/prefix/fileA.jsonl".into()),
-                    url_decoded_key: Some("some/prefix/fileA.parquet".into()),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            ..Default::default()
-        };
-        assert_eq!(suffix_from_record(&jsonl), RecordType::Parquet);
+    async fn cdf_test_setup() -> DeltaResult<(SessionContext, CdfLoadBuilder)> {
+        let table = deltalake::open_table("../../tests/data/hive/checkpoint-cdf-table/").await?;
+        let cdf = DeltaOps::from(table).load_cdf().with_starting_version(3);
+        let ctx = SessionContext::new();
+        Ok((ctx, cdf))
+    }
+
+    #[tokio::test]
+    async fn test_read_cdf() -> DeltaResult<()> {
+        let (ctx, cdf) = cdf_test_setup().await?;
+        let provider = DeltaCdfTableProvider::try_new(cdf)?;
+        let df = retrieve_change_data(&ctx, provider).await?;
+
+        assert_batches_sorted_eq!(
+            [
+                "+----+--------+------------+",
+                "| id | name   | birthday   |",
+                "+----+--------+------------+",
+                "| 13 | Ryan   | 2023-12-22 |",
+                "| 14 | Zach   | 2023-12-25 |",
+                "| 12 | Nick   | 2023-12-29 |",
+                "| 11 | Ossama | 2024-12-30 |",
+                "| 12 | Ossama | 2024-12-30 |",
+                "| 15 | Zach   | 2023-12-25 |",
+                "| 14 | Ryan   | 2023-12-22 |",
+                "| 13 | Nick   | 2023-12-29 |",
+                "+----+--------+------------+",
+            ],
+            &df.collect().await?
+        );
+        Ok(())
+    }
+
+    /// This test will ensure that CSV output can be correctly written to the registered object
+    /// store
+    #[tokio::test]
+    async fn test_write_csv() -> DeltaResult<()> {
+        let (ctx, cdf) = cdf_test_setup().await?;
+        let store = Arc::new(object_store::memory::InMemory::new());
+        ctx.register_object_store(&Url::parse("cdfo://").unwrap(), store.clone());
+
+        let mut stream = store.list(None);
+        while let Some(Ok(_entry)) = stream.next().await {
+            unreachable!("The InMemory object store should be empty before the test begins");
+        }
+
+        let provider = DeltaCdfTableProvider::try_new(cdf)?;
+        let change_data = retrieve_change_data(&ctx, provider).await?;
+        change_data
+            .write_csv("cdfo://", DataFrameWriteOptions::default(), None)
+            .await?;
+
+        let mut stream = store.list(None);
+        let mut output_found = false;
+        while let Some(Ok(entry)) = stream.next().await {
+            println!("entry: {entry:?}");
+            output_found = true;
+        }
+
+        assert!(output_found, "Nothing was found in the prefix!");
+        Ok(())
     }
 }

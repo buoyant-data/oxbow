b9131b97635fcf11673fcff683828ce2985e1e6b
R. Tyler Croy <rtyler@buoyantdata.com>
2026-04-07 17:29:42 +0000

    Allow the 1.9.0 release through github actions

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -20,18 +20,18 @@ PLATFORMS=("x86-64" "arm64")
 for d in $(ls ./lambdas); do
         if [ ! -d "./lambdas/${d}" ]; then
                 echo ">> ${d} is not a directory? por que?";
-                exit 1
+        else
+                echo ">> Building release for ${d}"
+
+                # The package name for the oxbow lambda is not just "oxbow" because it would collide with the crate named "oxbow"
+                if [ "${d}" = "oxbow" ]; then
+                        d="oxbow-lambda";
+                fi
+
+                for p in ${PLATFORMS[@]}; do
+                        echo ">> ${p}"
+                        time cargo lambda build --package ${d} --release --output-format zip --${p} --lambda-dir target/lambda/${p}
+                        mv target/lambda/${p}/${d}/bootstrap.zip target/lambda/${d}-${p}.zip
+                done;
         fi;
-        echo ">> Building release for ${d}"
-
-        # The package name for the oxbow lambda is not just "oxbow" because it would collide with the crate named "oxbow"
-        if [ "${d}" = "oxbow" ]; then
-                d="oxbow-lambda";
-        fi
-
-        for p in ${PLATFORMS[@]}; do
-                echo ">> ${p}"
-                time cargo lambda build --package ${d} --release --output-format zip --${p} --lambda-dir target/lambda/${p}
-                mv target/lambda/${p}/${d}/bootstrap.zip target/lambda/${d}-${p}.zip
-        done;
 done;
